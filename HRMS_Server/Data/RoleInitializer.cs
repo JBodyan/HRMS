b579bf7086c3d3394dbd9b45a9a61d27b07cd49c
JBodyan <jstix.bogdan.kandela@gmail.com>
2019-07-19 02:07:50 +0300

    Role initializer works

---
+++ HRMS_Server/Data/RoleInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRMS_Server.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace HRMS_Server.Data
+{
+    public class RoleInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            var adminEmail = "admin@gmail.com";
+            var password = "Admin123456";
+            if (await roleManager.FindByNameAsync("Admin") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+            if (await roleManager.FindByNameAsync("Manager") == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole("Manager"));
+            }
+            if (await userManager.FindByNameAsync(adminEmail) == null)
+            {
+                var admin = new User { Email = adminEmail, UserName = "admin" };
+                var result = await userManager.CreateAsync(admin, password);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, "Admin");
+                }
+            }
+        }
+    }
+}
