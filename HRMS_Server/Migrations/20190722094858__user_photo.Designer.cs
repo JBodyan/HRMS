234119ae7e55914bc6f055d956e37862238fd039
JBodyan <jstix.bogdan.kandela@gmail.com>
2019-07-22 13:46:45 +0300

    Autorization JWT by roles now works

--- HRMS_Server/Migrations/20190722094858__user_photo.Designer.cs
+++ HRMS_Server/Migrations/20190722094858__user_photo.Designer.cs
@@ -4,14 +4,16 @@ using HRMS_Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HRMS_Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190722094858__user_photo")]
+    partial class _user_photo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
